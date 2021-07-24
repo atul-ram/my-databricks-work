d9d17c85e2abdd2e8718049379737695171d5b32
atul <atul.ram@gmail.com>
2021-07-24 21:29:58 +0000

    ensure token works

--- main.tf
+++ main.tf
@@ -17,7 +17,12 @@ terraform {
 # PROVIDERS
 
 provider "databricks" {
-
+  host  = var.host
+  token = var.token
+  azure_subscription_id       = ""
+  azure_client_secret         = ""
+  azure_client_id             = ""
+  azure_tenant_id             = ""
 }
 
 resource "databricks_user" "my-user" {
