bb668158863fc3560fd642cbae8bdfd94a02228c
atul sahu <atul.sahu@nl.abnamro.com>
2021-07-25 08:46:25 +0200

    1

--- script.sh
+++ script.sh
@@ -1,4 +1,9 @@
-terraform init -backend-config="azure.conf"
+# clouddrive mount -s mySubscription -g sandbox-rg -n terraformdemofiles  -f cloudshell-logs
+
+#git config --global user.email "atul.ram@gmail.com"
+#git config --global user.name "atul"
+
+terraform init -backend-config="backend.tfvars"
 #terraform init -backend-config="azure.conf"  -reconfigure
 terraform plan -out=tfplan
 terraform apply -auto-approve -input=false
